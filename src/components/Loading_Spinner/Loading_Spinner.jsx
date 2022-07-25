1acf86508dc3192d57d80caee0577477c5b58aaa
BetterLogic Team <102576102+BetterLogicTeam@users.noreply.github.com>
2022-07-25 06:20:59 -0700

    Last commit

---
+++ src/components/Loading_Spinner/Loading_Spinner.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Spinner_Loading.css'
+
+export default function Loading_Spinner() {
+  return (
+    <div>
+
+<div class="Loading_data">
+        <span>L</span>
+        <span>O</span>
+        <span>A</span>
+        <span>D</span>
+        <span>I</span>
+        <span>N</span>
+        <span>G</span>
+    </div>
+    </div>
+  )
+}
